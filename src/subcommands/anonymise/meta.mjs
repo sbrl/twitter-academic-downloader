14b49cca2dee0ec521a47cc503089c889bd73605
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-05-14 17:35:08 +0100

    anonymise: clarify in cli docs

--- src/subcommands/anonymise/meta.mjs
+++ src/subcommands/anonymise/meta.mjs
@@ -1,7 +1,7 @@
 "use strict";
 
 export default function(cli) {
-	cli.subcommand("anonymise", "Anonymises tweets in a jsonl file. Output file is <filename_without_ext>-anonymised.<ext>.")
+	cli.subcommand("anonymise", "Anonymises tweets in a jsonl file. Output file is <filename_without_ext>-anonymised.<ext>. Note that this is done automatically on download - you do NOT need to run this manually unless you have data from elsewhere.")
 		.argument("input", "Path to the input file to anonymise.", null, "string")
 		.argument("type", "The type of object to anonymise. Default: tweet. Possible values: tweet, user.", "tweet", "string")
 		.argument("credentials", "Path to a (TOML formatted) credentials file that contains the salt to use for anonymisation. See the README for more information on the format.")
