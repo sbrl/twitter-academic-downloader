84e3b9e27143af869ba9b37cd37ebbd2cc853dc2
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2021-07-26 19:52:01 +0100

    download-media: make variables local, add progress feedback via stdout

--- download-media.sh
+++ download-media.sh
@@ -58,6 +58,8 @@ fi
 delay="1";	# The delay between requests - in seconds
 curl_user_agent="twitter-academic-downloader (Bash; $(uname) $(arch); +https://www.npmjs.com/package/twitter-academic-downloader) curl/$(curl --version | awk '{ print $2; exit }')";
 
+downloaded_count="0";
+
 ###############################################################################
 
 # Extracts the URLs from tweet JSON objects read from stdin.
@@ -88,6 +90,10 @@ optimise_image() {
 # Downloads a single item to the current directory - but only if the filename doesn't already exist.
 # $1	The URL of the image to download.
 download_single() {
+	local url; local filename;
+	local time_start; local time_elapsed;
+	local time_to_wait;
+	
 	url="${1}";
 	filename="$(basename "${url}")";
 	
@@ -102,6 +108,9 @@ download_single() {
 	
 	deface -o "${filename}" "${filename}";	# Blur faces
 	optimise_image "${filename}";			# Optimise the image to reduce filesize
+	
+	downloaded_count="$((downloaded_count + 1))";
+	echo -ne "${downloaded_count} downloaded so far; latest: ${url}\r";
 	
 	# Calculate the time remaining we need to wait before making the next request
 	time_elapsed="$((($(date +%s%N) - time_start) / 1000000))";
